c25600fd0b41694997f992302aad019fa4b61c88
T.M <tomasz.piotr.malinowski@gmail.com>
2018-08-31 15:53:43 +0200

    initial commit

---
+++ RootModelValidationDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RootModelValidationDemo.Validation;
+
+namespace RootModelValidationDemo.Pages
+{
+    [BindProperties]
+    [CustomValidator]
+    public class IndexModel : PageModel
+    {
+        public string Name { get; set; }
+        
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                // never gets hit, even if Name is invalid
+                return Page();
+            }
+            return Page();
+        }
+
+
+        public class Validator : AbstractValidator<IndexModel>
+        {
+            public Validator()
+            {
+                RuleFor(x => x.Name).Must(x => x.StartsWith("qwerty")).WithMessage("Name must start with \"qwerty\"");
+            }
+
+            public override ValidationResult Validate(ValidationContext<IndexModel> context)
+            {
+                return base.Validate(context); // never gets executed
+            }
+
+            public override Task<ValidationResult> ValidateAsync(ValidationContext<IndexModel> context, CancellationToken cancellation = default(CancellationToken))
+            {
+                return base.ValidateAsync(context, cancellation); // never gets executed
+            }
+        }
+    }
+}
---
+++ RootModelValidationDemo/Validation/CustomValidatorAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RootModelValidationDemo.Validation
+{
+    public class CustomValidatorAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return false; // never gets executed
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return new ValidationResult("Validation error", new[] { "Name" }); // never gets executed
+        }
+    }
+}
